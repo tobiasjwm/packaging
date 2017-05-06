e5bf4213c9978382ad22a0ef34fbffa761b0eca4
Tobias Morrison <tobias@globalmacit.com>
2017-05-05 22:29:06 -0400

    test custom processor for workflow zips
    Trying to zip up automator workflows and use custom processor to unzip
    and place the workflows to retain spaces in name.

---
+++ luggage-packages/xkpassword/tar-workflows.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Companion script for The Luggage packaging of XKPasswd
+
+# Tar up the Automator workflows
+
+/usr/bin/tar cvjf Best_Password.workflow.tar.bz2 "Best Password.workflow"
+
+/usr/bin/tar cvjf Better_Password.workflow.tar.bz2 "Better Password.workflow"
+
+/usr/bin/tar cvjf Good_Password.workflow.tar.bz2 "Good Password.workflow"
