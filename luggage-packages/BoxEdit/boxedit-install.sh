246c473484061fda32c3f36746f5287f9aca49e7
Tobias Morrison <tobias@globalmacit.com>
2017-08-11 17:05:27 -0400

    initial commit of Box Edit installer

--- luggage-packages/BoxEdit/boxedit-install.sh
+++ luggage-packages/BoxEdit/boxedit-install.sh
@@ -9,16 +9,16 @@ target="$3"
 ourUser="$(whoami)"
 
 # Directory variables
-appDir="${target}/Applications/Install\ Box\ Tools.app/Contents/Resources"
-tgtDir="${target}/Users/$ourUser/Library/Application\ Support/Box/Box\ Edit"
+appDir="${target}/Applications/Install Box Tools.app/Contents/Resources"
+tgtDir="${target}/Users/$ourUser/Library/Application Support/Box/Box Edit"
 
 # App variables, because there are spaces
-boxEdit="Box\ Edit.app"
-boxSvr="Box\ Local\ Com\ Server.app"
+boxEdit="Box Edit.app"
+boxSvr="Box Local Com Server.app"
 
 # Version checks
-appVer=`/usr/bin/defaults read "$appDir/$boxEdit/Contents/Info.plist" CFBundleShortVersionString`
-userVer=`/usr/bin/defaults read "$tgtDir/$boxEdit/Contents/Info.plist" CFBundleShortVersionString`
+#appVer=`/usr/bin/defaults read "$appDir/$boxEdit/Contents/Info.plist" CFBundleShortVersionString`
+#userVer=`/usr/bin/defaults read "$tgtDir/$boxEdit/Contents/Info.plist" CFBundleShortVersionString`
 
 
 # Function to create our directory
@@ -33,22 +33,19 @@ createDir() { # path mode
 copyApps() { # srcdir destdir srcItem1 srcItem2
 	# Check if our directory exists and create it if it does not
     if [ -e "${target}/$1/$3" ]; then
-    	if [ appVer <= userVer ]; then
-    		/bin/cp "$1/$3" "$2/$3"
-    		/bin/cp "$1/$4" "$2/$4"
-    	else
-    		/bin/echo "Newer version already installed. Exiting."
-    	fi
+    	/bin/cp -R "$1/$3" "$2/$3"
+    	/bin/cp -R "$1/$4" "$2/$4"
     else
     	/bin/echo "Install Box Tools.app is not installed on this device."
     fi
 }
 
+# Create directory
+createDir "$tgtDir" 755
 
+# Copy apps to local user folder
+copyApps "$appDir" "$tgtDir" "$boxEdit" "$boxSvr"
 
-# unit tests
-/bin/echo $ourUser
-/bin/echo $appDir
-/bin/echo $tgtDir
-/bin/echo $appVer
-/bin/echo $userVer
+# Launch apps to activate
+/usr/bin/open "${target}/$tgtDir/$boxEdit"
+/usr/bin/open "${target}/$tgtDir/$boxSvr"
