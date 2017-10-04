552084534a120b9e16ce66335105180654821d7b
Tobias Morrison <tobias@globalmacit.com>
2017-10-04 10:54:41 -0400

    create function to add Yo! command

--- luggage-packages/yo/notification_scripts/boxdrivenotifier.sh
+++ luggage-packages/yo/notification_scripts/boxdrivenotifier.sh
@@ -8,21 +8,33 @@
 # send a message to end-points to prompt employees to check out Box Drive.
 #! requires Yo! > 2.0 https://github.com/sheagcraig/yo
 
-# Set our variables
+# Messaging variables. Change the items in quotes to suit your needs.
+
+TITLE="Box Drive Now Available"
+SUBTITLE="A Better Way to Access Your Box Files"
+INFO="Click to find out more."
+BUTTON="More Info…"
+ACTION="https://globalmac-it.itglue.com/DOC-1673628-1147218"
+
+# Set our static variables
 
 DRIVE="/Applications/Box.app"
 SCHED="/usr/local/bin/yo_scheduler"
+
+# function to run our commands
+
+yo_action {
+	/usr/local/bin/yo_scheduler \
+	--title "$TITLE" \
+	--subtitle "$SUBTITLE" \
+	--info "$INFO" \
+	--action-btn "$BUTTON" \
+	--action-path "$ACTION"
 
 # Check for Box Drive before setting message for delivery
 
 if [ -e "$DRIVE" ] ; then
 	exit 1
 else
-	# this should be made into a function
-	$SCHED \
-	--title 'Box Drive Now Available' \
-	--subtitle 'A Better Way to Access Your Box Files' \
-	--info 'Click to find out more.' \
-	--action-btn 'More Info…' \
-	--action-path 'https://globalmac-it.itglue.com/DOC-1673628-1147218'
-	
+	yo_action
+fi
