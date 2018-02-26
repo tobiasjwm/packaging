a3e8e9d44872a101a72413a5b1b66c5b7b439372
Tobias Morrison <tobias@globalmacit.com>
2018-02-26 13:10:12 -0500

    Clean up formatting and add exit 0

--- munki_scripts/Conditions/BoxSyncDetector.sh
+++ munki_scripts/Conditions/BoxSyncDetector.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
-# munki admin-provided conditional. 
-# thank you to @eholtam on MacAdmins Slack. 
-# https://macadmins.slack.com/archives/C04QVPFGU/p1518538489000816
+#	munki admin-provided conditional. 
+#	thank you to @eholtam on MacAdmins Slack. 
+#	https://macadmins.slack.com/archives/C04QVPFGU/p1518538489000816
 
 plistLocation="/Library/Managed Installs/ConditionalItems"
 applicationLocation="/Applications/Box Sync.app"
 prefName=isBoxSyncInstalled
 
-#Verify a user is logged in
+#	Check to see if Box Sync is installed
 if [ -e "${applicationLocation}" ]
-then
-    defaults write "${plistLocation}" "${prefName}" -bool true
-else
-    defaults write "${plistLocation}" "${prefName}" -bool false
+	then
+    	defaults write "${plistLocation}" "${prefName}" -bool true
+	else
+    	defaults write "${plistLocation}" "${prefName}" -bool false
 fi
+#	Since 'defaults' outputs a binary plist, we need to ensure that munki can read it by 
+#!	converting it to xml 
 plutil -convert xml1 ${plistLocation}".plist
+
+exit 0
