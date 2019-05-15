246d32f2e1f13982bbbea3f45e72cd61b8e2ac8e
Tobias Morrison <tobias@globalmacit.com>
2019-05-15 11:34:58 -0400

    Added WealthCounselRibbon installer
    Had to build this because the vendor thought they were being fancy by building an application installer, which was just a crappy Cocoa wrapper around a script.

---
+++ munkipkg-packages/WealthCounselRibbon/munki_preinstall.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+## This is a preinstall_script for a pkginfo file to not install the item unless a user is logged in.
+## Warning: Absolutely be sure that you don't make this a preinstall_script for an item that requires a logout or reboot in order to install!!!
+
+# Get the state of the lastuser
+lastUser=$(/usr/bin/defaults read /Library/Preferences/com.apple.loginwindow lastUser)
+
+# Check if the last user is logged in...
+if [ ! -z "$lastUser" ] && [ "$lastUser" == "loggedIn" ]; then
+
+  # If the last user is logged in, proceed...
+  exit 0
+   
+else
+  
+  # Otherwise abort installing this item
+   exit 1
+   
+fi
