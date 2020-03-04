4ecb1426bb3771593fb324c417ae040fab6b2ebc
Tobias Morrison <t@cmndhlp.com>
2020-03-04 16:46:19 -0500

    removed file test cascade
    Pulled the initial file test into a separate test.
    
    New flow: Check for config file. Exit if not present. Then check version.

--- munki_scripts/Carbonite/Carbonite-installcheck_script.sh
+++ munki_scripts/Carbonite/Carbonite-installcheck_script.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 
 # This script does not deal with a version greater than the target. We will end up 
 # hoping that installer will reject the install is a newer version is installed.
@@ -7,16 +7,20 @@
 
 # REPLACE with current installer version
 installerVersion="10.3.5.236"
+dcprotectSettings="/Library/Application Support/DCProtect/Service/LocalServiceSettings.config.xml"
 
 
 # This will check Carbonite's odd versioning scheme.
-carboniteVersion=$(grep  ClientVersion "/Library/Application Support/DCProtect/Service/LocalServiceSettings.config.xml" | tr -d '\t' | sed 's/^<.*>\([^<].*\)<.*>$/\1/' | cut -f2 -d ">" | cut -f1 -d "<")
+carboniteVersion=$(grep  ClientVersion "$dcprotectSettings" | tr -d '\t' | sed 's/^<.*>\([^<].*\)<.*>$/\1/' | cut -f2 -d ">" | cut -f1 -d "<")
 
 # Compare and determine if we have the same version installed.
-if [ -e "/Library/Application Support/DCProtect/Service/LocalServiceSettings.config.xml" ]; then
-	if [ "$carboniteVersion" == "$installerVersion" ]; then
-		echo "Carbonite version $installerVersion already installed. Skipping."
-		exit 1
+if [ ! -f "$dcprotectSettings" ]; then
+	echo "Carbonite is not installed. Exiting."
+	exit 1
+
+if [ "$carboniteVersion" == "$installerVersion" ]; then
+	echo "Carbonite version $installerVersion already installed. Skipping."
+	exit 2
 else
 	echo "Carbonite version $installerVersion not installed. Installing."
 	exit 0
