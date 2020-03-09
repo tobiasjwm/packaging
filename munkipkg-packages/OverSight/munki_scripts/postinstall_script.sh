9750612303d506589b86fbfd7e711b93808e778e
Tobias Morrison <t@cmndhlp.com>
2020-03-09 17:13:23 -0400

    add OverSight

---
+++ munkipkg-packages/OverSight/munki_scripts/postinstall_script.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# munki postinstall_script for OverSight.app
+/Library/Management/bin/OverSight_Installer.app/Contents/MacOS/OverSight_Installer -install
---
+++ munkipkg-packages/OverSight/munki_scripts/uninstall_script.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# munki uninstall_script for OverSight.app
+/Library/Management/bin/OverSight_Installer.app/Contents/MacOS/OverSight_Installer -uninstall
