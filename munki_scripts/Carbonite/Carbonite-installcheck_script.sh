80c7f33bfd112025c0a451a7254cbf9140944905
Tobias Morrison <t@cmndhlp.com>
2020-03-13 15:57:11 -0400

    creating a functioning install_check script
    script was failing to use the path to file variable properly. yanked it and put the path directly in the file test.

--- munki_scripts/Carbonite/Carbonite-installcheck_script.sh
+++ munki_scripts/Carbonite/Carbonite-installcheck_script.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/sh
 
 # This script does not deal with a version greater than the target. We will end up 
 # hoping that installer will reject the install is a newer version is installed.
@@ -6,23 +6,16 @@
 # another day.
 
 # REPLACE with current installer version
-installerVersion="10.3.5.236"
-dcprotectSettings="/Library/Application Support/DCProtect/Service/LocalServiceSettings.config.xml"
-
+installerVersion="10.4.0.51"
 
 # This will check Carbonite's odd versioning scheme.
-carboniteVersion=$(grep  ClientVersion "$dcprotectSettings" | tr -d '\t' | sed 's/^<.*>\([^<].*\)<.*>$/\1/' | cut -f2 -d ">" | cut -f1 -d "<")
+carboniteVersion=$(grep  ClientVersion "/Library/Application Support/DCProtect/Service/LocalServiceSettings.config.xml" | tr -d '\t' | sed 's/^<.*>\([^<].*\)<.*>$/\1/' | cut -f2 -d ">" | cut -f1 -d "<")
 
 # Compare and determine if we have the same version installed.
-if [ ! -f "$dcprotectSettings" ]; then
-	echo "Carbonite is not installed. Exiting."
-	exit 1
-
-if [ "$carboniteVersion" == "$installerVersion" ]; then
+if [ "$carboniteVersion" = "$installerVersion" ]; then
 	echo "Carbonite version $installerVersion already installed. Skipping."
 	exit 2
 else
 	echo "Carbonite version $installerVersion not installed. Installing."
 	exit 0
-	fi
 fi
