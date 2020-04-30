c6f1689b5a8818e03a226d90d2fc25eba89d0ea8
Tobias Morrison <t@cmndhlp.com>
2020-04-30 15:35:48 -0400

    add `set -e` to overcome script continuation
    In the install command, even when the install completes, the following pops up: 2020-04-30 15:26:11.762 OverSight Helper[64316:2745015] Warning: Window move completed without beginning.
    It never ends and hijacks everything.

--- munkipkg-packages/OverSight/munki_scripts/postinstall_script.sh
+++ munkipkg-packages/OverSight/munki_scripts/postinstall_script.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
+
+# exit on any error. This is an attempt to overcome the fact that the vendor's 
+# install script does not exit gracefully.
+set -e
+
 # munki postinstall_script for OverSight.app
 /Library/Management/bin/OverSight_Installer.app/Contents/MacOS/OverSight_Installer -install
-
-if [ $? == "OVERSIGHT: install ok!" ]; then
-	exit 0
-else
-	exit 1
-fi
--- munkipkg-packages/OverSight/munki_scripts/uninstall_script.sh
+++ munkipkg-packages/OverSight/munki_scripts/uninstall_script.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+# exit on any error. This is an attempt to overcome the fact that the vendor's 
+# install script does not exit gracefully.
+set -e
+
 # munki uninstall_script for OverSight.app
 /Library/Management/bin/OverSight_Installer.app/Contents/MacOS/OverSight_Installer -uninstall
-exit 0
